0244b015cb15db7219fdb1ec2dc28ca4591e84cd
Prince Ntimeni <ntimza2000@gmail.com>
2024-04-04 13:54:33 +0200

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import com.tester.Tester;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        var test = new Test();
+        test.main(args);;
+        new Tester().main(args);
+    }
+}
---
+++ com/tester/Tester.java
@@ -0,0 +1,7 @@
+package com.tester;
+
+public class Tester {
+    public void main(String[] args) {
+        System.out.println("Tested !");
+    }
+}
